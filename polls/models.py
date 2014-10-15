9cb2e3bba81c4e91aa94a4205dd104cea1369f99
shaoweizhang <zsw316@163.com>
2014-10-15 16:54:53 +0800

    test pycharm commit

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,7 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+
     def __unicode__(self):
         return self.question
 
