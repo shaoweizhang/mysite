b016a33b0dd7aba6797163fa93c68bd3710f9ac9
shaoweizhang <zsw316@163.com>
2014-10-16 16:34:32 +0800

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls',namespace="polls")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
