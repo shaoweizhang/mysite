85bdfc286a2152acf948802d10682671e3089b5a
shaoweizhang <zsw316@163.com>
2014-10-17 15:41:22 +0800

    templates

--- polls/models.py
+++ polls/models.py
@@ -24,3 +24,19 @@ class Choice(models.Model):
 
     def __unicode__(self):
         return self.choice_text
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __unicode__(self): # __unicode__ on Python 2
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person)
+    group = models.ForeignKey(Group)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
