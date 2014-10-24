8a30b1d3c6743fadbd6f78f79bce9e7ffd1ec382
shaoweizhang <zsw316@163.com>
2014-10-24 12:12:09 +0800

    templates

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -8,7 +8,7 @@ from demo_app import views
 
 urlpatterns = patterns('',
 	url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
-    url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name="contact"),
+    url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name="demo_contact"),
     url(r'^form$', views.demo_form, name="demo_form"),
     url(r'^form_template$', views.demo_form_with_template, name="demo_form_with_template"),
     url(r'^form_inline$', views.demo_form_inline, name="demo_form_inline"),
@@ -16,8 +16,8 @@ urlpatterns = patterns('',
     url(r'^tabs$', views.demo_tabs, {}, name="demo_tabs"),
     url(r'^pagination$', views.demo_pagination, {}, name="demo_pagination"),
     url(r'^widgets$', views.demo_widgets, {}, name="demo_widgets"),
-    url(r'^buttons$', TemplateView.as_view(template_name='buttons.html'), name="buttons"),
-    url(r'^example$', TemplateView.as_view(template_name='base.html.example'), name="example"),
+    url(r'^buttons$', TemplateView.as_view(template_name='buttons.html'), name="demo_buttons"),
+    url(r'^example$', TemplateView.as_view(template_name='base.html.example'), name="demo_example"),
     #url(r'^example$', views.example, name="example"),
 )
 
