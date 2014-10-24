a417182cb5b0a8d2e112ca2d29cf4c948ae88ac2
shaoweizhang <zsw316@163.com>
2014-10-24 13:14:40 +0800

    templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,12 +84,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -9,3 +10,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bootstrap/', include('demo_app.urls', namespace="demo_app")),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+            url(r'^media/(?P<path>.*)$', "django.views.static.serve",  {"document_root": settings.MEDIA_ROOT,}),
+    )
