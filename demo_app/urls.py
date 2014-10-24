24a2b335d2d1b85c3a0a9aa2f521eb96162efe41
shaoweizhang <zsw316@163.com>
2014-10-24 12:08:51 +0800

    templates

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -4,26 +4,20 @@ from django.conf.urls import patterns, url
 # from django.contrib import admin
 # admin.autodiscover()
 from django.views.generic import TemplateView
+from demo_app import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'demo_project.views.home', name='home'),
-    # url(r'^demo_project/', include('demo_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+	url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
     url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name="contact"),
-    url(r'^form$', 'demo_app.views.demo_form'),
-    url(r'^form_template$', 'demo_app.views.demo_form_with_template'),
-    url(r'^form_inline$', 'demo_app.views.demo_form_inline'),
-    url(r'^formset$', 'demo_app.views.demo_formset', {}, "formset"),
-    url(r'^tabs$', 'demo_app.views.demo_tabs', {}, "tabs"),
-    url(r'^pagination$', 'demo_app.views.demo_pagination', {}, "pagination"),
-    url(r'^widgets$', 'demo_app.views.demo_widgets', {}, "widgets"),
+    url(r'^form$', views.demo_form, name="demo_form"),
+    url(r'^form_template$', views.demo_form_with_template, name="demo_form_with_template"),
+    url(r'^form_inline$', views.demo_form_inline, name="demo_form_inline"),
+    url(r'^formset$', views.demo_formset, {}, name="demo_formset"),
+    url(r'^tabs$', views.demo_tabs, {}, name="demo_tabs"),
+    url(r'^pagination$', views.demo_pagination, {}, name="demo_pagination"),
+    url(r'^widgets$', views.demo_widgets, {}, name="demo_widgets"),
     url(r'^buttons$', TemplateView.as_view(template_name='buttons.html'), name="buttons"),
+    url(r'^example$', TemplateView.as_view(template_name='base.html.example'), name="example"),
+    #url(r'^example$', views.example, name="example"),
 )
+
